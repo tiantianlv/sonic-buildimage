8e11628aef09034eb352d5715219ed6da32af4ce
Wirut Getbamrung <wgetbum@icloud.com>
2018-09-25 18:51:30 +0000

    Update sonic plugins to support platformutil

--- device/alibaba/x86_64-alibaba_as13-48f8h-cl-r0/plugins/fanutil.py
+++ device/alibaba/x86_64-alibaba_as13-48f8h-cl-r0/plugins/fanutil.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
-try:
-    from sonic_psu.psu_base import PsuBase
-except ImportError as e:
-    raise ImportError (str(e) + "- required module not found")
+import re
+import requests
 
-class FanUtil(PsuBase):
+class FanUtil():
     """Platform-specific FanUtil class"""
 
     def __init__(self):
-        PsuBase.__init__(self)
+
+        self.fan_fru_url = "http://[fe80::1:1%eth0.4088]:8080/api/sys/fruid/fan"
+        self.sensor_url = "http://[fe80::1:1%eth0.4088]:8080/api/sys/sensors"
+        self.fru_data_list = None
+        self.sensor_data_list = None
+
+
+    def request_data(self):
+        # Reqest data from BMC if not exist.
+        if self.fru_data_list is None or self.sensor_data_list is None:
+            fru_data_req = requests.get(self.fan_fru_url)
+            sensor_data_req = requests.get(self.sensor_url)
+            fru_json = fru_data_req.json()
+            sensor_json = sensor_data_req.json()
+            self.fru_data_list = fru_json.get('Information')
+            self.sensor_data_list = sensor_json.get('Information')
+        return self.fru_data_list, self.sensor_data_list
 
 
     def get_num_fans(self):     
@@ -17,16 +31,38 @@ class FanUtil(PsuBase):
             Get the number of fans
             :return: int num_fans
         """
-        num_fans = 4
+        num_fans = 8
 
         return num_fans
+
 
     def get_fan_speed(self, index):
         """
             Get the current speed of the fan, the unit is "RPM"  
             :return: int fan_speed
         """
-        fan_speed = 105600
+
+        # Set key and index.
+        fan_speed = 0
+        position_key = "Front" if index % 2 !=0 else "Rear"
+        index = int(round(float(index)/2))
+        fan_key = "Fan " + str(index) + " " + position_key
+
+        try:
+            # Request and validate fan information.
+            self.fru_data_list, self.sensor_data_list = self.request_data()
+
+            # Get fan's speed.
+            for sensor_data in self.sensor_data_list:
+                sensor_name = sensor_data.get('name') 
+                if "fan" in str(sensor_name):
+                    fan_data = sensor_data.get(fan_key)
+                    fan_sp_list = map(int, re.findall(r'\d+', fan_data))
+                    fan_speed = fan_sp_list[0]
+
+        except:
+            print "Error: Unable to access fan speed information"
+            return "N/A"
 
         return fan_speed
 
@@ -38,9 +74,31 @@ class FanUtil(PsuBase):
             the status of the fan is not ok.
             :return: int fan_low_threshold
         """
-        fan_low_threshold = 300
+
+        # Set key and index.
+        fan_low_threshold = 0
+        position_key = "Front" if index % 2 !=0 else "Rear"
+        index = int(round(float(index)/2))
+        fan_key = "Fan " + str(index) + " " + position_key
+
+        try:
+            # Request and validate fan information.
+            self.fru_data_list, self.sensor_data_list = self.request_data()
+
+            # Get fan's threshold.
+            for sensor_data in self.sensor_data_list:
+                sensor_name = sensor_data.get('name') 
+                if "fan" in str(sensor_name):
+                    fan_data = sensor_data.get(fan_key)
+                    fan_sp_list = map(int, re.findall(r'\d+', fan_data))
+                    fan_low_threshold = fan_sp_list[1]
+
+        except:
+            print "Error: Unable to access fan threshold information"
+            return "N/A"
 
         return fan_low_threshold
+
 
     def get_fan_high_threshold(self, index):
         """
@@ -49,24 +107,85 @@ class FanUtil(PsuBase):
             the status of the fan is not ok
             :return: int fan_high_threshold
         """
-        fan_high_threshold = 16000
+
+        # Set key and index.
+        fan_high_threshold = 0
+        position_key = "Front" if index % 2 !=0 else "Rear"
+        index = int(round(float(index)/2))
+        fan_key = "Fan " + str(index) + " " + position_key
+
+        try:
+            # Request and validate fan information.
+            self.fru_data_list, self.sensor_data_list = self.request_data()
+
+            # Get fan's threshold.
+            for sensor_data in self.sensor_data_list:
+                sensor_name = sensor_data.get('name') 
+                if "fan" in str(sensor_name):
+                    fan_data = sensor_data.get(fan_key)
+                    fan_sp_list = map(int, re.findall(r'\d+', fan_data))
+                    fan_high_threshold = fan_sp_list[2]
+
+        except:
+            print "Error: Unable to access fan threshold information"
+            return "N/A"
 
         return fan_high_threshold
+
 
     def get_fan_pn(self, index):
         """
             Get the product name of the fan
             :return: str fan_pn
         """
-        fan_pn = "M6510-FAN-F"
+
+        # Set key and index.
+        fan_pn = "N/A"
+        index = int(round(float(index)/2))
+        fan_fru_key = "FAN" + str(index) + " FRU"
+        
+        try:
+            # Request and validate fan information.
+            self.fru_data_list, self.sensor_data_list = self.request_data()
+
+            # Get fan's fru.
+            for fan_fru in self.fru_data_list:
+                matching_fan = [s for s in fan_fru if fan_fru_key in s]
+                if matching_fan:
+                    serial = [s for s in fan_fru if "Product" in s]
+                    fan_pn = serial[0].split()[4]                 
+
+        except:
+            print "Error: Unable to access product number information"
+            return "N/A"
 
         return fan_pn
+
 
     def get_fan_sn(self, index):
         """
             Get the serial number of the fan
             :return: str fan_sn
         """
-        fan_sn = "1000000000014"
+
+        # Set key and index.
+        fan_sn = "N/A"
+        index = int(round(float(index)/2))
+        fan_fru_key = "FAN" + str(index) + " FRU"        
+        
+        try:
+            # Request and validate fan information.
+            self.fru_data_list, self.sensor_data_list = self.request_data()
+
+            # Get fan's fru.
+            for fan_fru in self.fru_data_list:
+                matching_fan = [s for s in fan_fru if fan_fru_key in s]
+                if matching_fan:
+                    serial = [s for s in fan_fru if "Serial" in s]
+                    fan_sn = serial[0].split()[4]                 
+
+        except:
+            print "Error: Unable to access serial number information"
+            return "N/A"
 
         return fan_sn
--- device/alibaba/x86_64-alibaba_as13-48f8h-cl-r0/plugins/psuutil.py
+++ device/alibaba/x86_64-alibaba_as13-48f8h-cl-r0/plugins/psuutil.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-import os.path
-import subprocess
-import sys
-import re
 import requests
 
 try:
@@ -15,8 +11,25 @@ class PsuUtil(PsuBase):
     """Platform-specific PSUutil class"""
 
     def __init__(self):
-        self.psu_restful_url = "http://192.168.1.10:8080/api/sys/fruid/status"
         PsuBase.__init__(self)
+        self.fru_status_url = "http://[fe80::1:1%eth0.4088]:8080/api/sys/fruid/status"
+        self.psu_info_url = "http://[fe80::1:1%eth0.4088]:8080/api/sys/fruid/psu"
+        
+        self.fru_status_list = None
+        self.psu_info_list = None
+
+
+    def request_data(self):
+        # Reqest data from BMC if not exist.
+        if self.fru_status_list is None or self.psu_info_list is None:
+            fru_status_req = requests.get(self.fru_status_url)
+            psu_info_req = requests.get(self.psu_info_url)
+            fru_status_json = fru_status_req.json()
+            psu_info_json = psu_info_req.json()
+            self.fru_status_list = fru_status_json.get('Information')
+            self.psu_info_list = psu_info_json.get('Information')
+        return self.fru_status_list, self.psu_info_list
+
 
     def get_num_psus(self):
         """
@@ -32,17 +45,18 @@ class PsuUtil(PsuBase):
         :param index: An integer, 1-based index of the PSU of which to query status
         :return: Boolean, True if PSU is operating properly, False if PSU is faulty
         """
-
+        
+        # init data
         psu_key = "PSU" + str(index)
         psu_status_key = "Power Status"
         psu_power_status = False
 
         try:
-            r = requests.get(self.psu_restful_url)
-            json_response = r.json()
-            fru_status_list = json_response.get('Information')
+            # Request and validate sensor's information
+            self.fru_status_list, self.psu_info_list = self.request_data()
 
-            for fru_status in fru_status_list:            
+            # Get PSU power status.
+            for fru_status in self.fru_status_list:            
                 is_psu = fru_status.get(psu_key)
                 psu_status  = str(fru_status.get(psu_status_key)).strip()
 
@@ -50,7 +64,7 @@ class PsuUtil(PsuBase):
                     psu_power_status = True
 
         except:
-            print "Error: Unable to access PSU status"
+            print "Error: Unable to access PSU power status"
             return False
 
         return psu_power_status
@@ -63,16 +77,17 @@ class PsuUtil(PsuBase):
         :return: Boolean, True if PSU is plugged, False if not
         """
 
+        # Init data
         psu_key = "PSU" + str(index)
         psu_presence_key = "Present"
         psu_presence_status = False
 
         try:
-            r = requests.get(self.psu_restful_url)
-            json_response = r.json()
-            fru_status_list = json_response.get('Information')
+            # Request and validate sensor's information.
+            self.fru_status_list, self.psu_info_list = self.request_data()
 
-            for fru_status in fru_status_list:            
+            # Get PSU present status.
+            for fru_status in self.fru_status_list:            
                 is_psu = fru_status.get(psu_key)
                 psu_status  = str(fru_status.get(psu_presence_key)).strip()
 
@@ -80,7 +95,63 @@ class PsuUtil(PsuBase):
                     psu_presence_status = True
 
         except:
-            print "Error: Unable to access PSU status"
+            print "Error: Unable to access PSU presence status"
             return False
 
         return psu_presence_status
+
+    def get_psu_sn(self, index):
+        """
+        Get the serial number of the psu,
+        
+        :param index: An integer, 1-based index of the PSU.
+        :return: Serial number
+        """
+        serial_number = "N/A"
+        psu_key = "PSU" + str(index) + " FRU"
+        psu_sn_key = "Serial Number"
+      
+        try:
+            # Request and validate sensor's information.
+            self.fru_status_list, self.psu_info_list = self.request_data()
+
+            # Get PSU fru info.
+            for psu_fru in self.psu_info_list:            
+                psu_sn  = str(psu_fru.get(psu_sn_key)).strip()
+                if psu_fru.get(psu_key) is not None:
+                    serial_number = psu_sn
+                    break
+
+        except:
+            print "Error: Unable to access PSU product number information"
+            return "N/A"
+
+        return serial_number
+
+    def get_psu_pn(self, index):
+        """
+        Get the product name of the psu
+        
+        :param index: An integer, 1-based index of the PSU.
+        :return: Product name
+        """
+        product_name = "N/A"
+        psu_key = "PSU" + str(index) + " FRU"
+        psu_pn_key = "Product Name"
+      
+        try:
+            # Request and validate sensor's information
+            self.fru_status_list, self.psu_info_list = self.request_data()
+
+            # Get PSU fru info.
+            for psu_fru in self.psu_info_list:         
+                psu_pn  = str(psu_fru.get(psu_pn_key)).strip()
+                if psu_fru.get(psu_key) is not None:
+                    product_name = psu_pn
+                    break
+
+        except:
+            print "Error: Unable to access PSU serial number information"
+            return "N/A"
+
+        return product_name
